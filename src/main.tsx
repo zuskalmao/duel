a29620de800d284e32623d50451adf03d9d5477b
zuskalmao <zuskamalovich@gmail.com>
2025-03-17 02:14:19 -0400

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+// Import wallet adapter CSS
+import '@solana/wallet-adapter-react-ui/styles.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
